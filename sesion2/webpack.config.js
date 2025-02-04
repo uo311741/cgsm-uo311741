33bbdeddc0708baeb68b1f26bf2eaa1c106ac7ce
uo311741 <uo311741@uniovi.es>
2025-02-04 17:42:06 +0100

    updates webpack

--- sesion2/webpack.config.js
+++ sesion2/webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
         "prac2-3":"./src/prac2-3.js",
         "prac2-4":"./src/prac2-4.js",
         "prac2-5":"./src/prac2-5.js",
+        "prac2-6":"./src/prac2-6.js",
+        "prac2-7":"./src/prac2-7.js",
     },
     devtool: 'inline-source-map',
     devServer: {
