eb39953f321cfab95640d97d772f2c259444527c
uo311741 <uo311741@uniovi.es>
2025-02-06 20:11:22 +0100

    session3

---
+++ sesion3/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mode: "development",
+    entry: {
+        "prac3-1":"./src/prac3-1.js",
+        "prac3-2":"./src/prac3-2.js",
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: __dirname
+        },
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000
+    }
+};
