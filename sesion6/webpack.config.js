352ca27fc59d9b1e307f7a98972344d0af2d63fc
uo311741 <uo311741@uniovi.es>
2025-03-07 10:27:54 +0100

    initializations

---
+++ sesion6/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: "development",
+    entry: {
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: __dirname
+        },
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000
+    }
+};
